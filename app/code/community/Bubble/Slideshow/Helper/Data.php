c4fa8076b19bacbd862f0dee2c7de98b021f6167
Johann Reinke <johann.reinke@gmail.com>
2014-04-04 22:41:23 +0200

    Tag v1.0.4

--- app/code/community/Bubble/Slideshow/Helper/Data.php
+++ app/code/community/Bubble/Slideshow/Helper/Data.php
@@ -4,7 +4,7 @@
  *
  * @category    Bubble
  * @package     Bubble_Slideshow
- * @version     1.0.3
+ * @version     1.0.4
  * @copyright   Copyright (c) 2013 BubbleCode (http://shop.bubblecode.net)
  */
 class Bubble_Slideshow_Helper_Data extends Mage_Core_Helper_Abstract
